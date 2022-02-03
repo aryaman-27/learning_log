6e2a10ba61a9da4f94076c492a44114d3b9b2e0d
aryaman-27 <chaudhary.aryaman07@gmail.com>
2022-02-03 16:08:13 +0530

    Initial commit with Model

---
+++ learning_logs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Topics(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
